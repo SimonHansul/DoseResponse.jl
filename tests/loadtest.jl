f50ac4fc25ee095df2f700ab992fb82a8fdad92e
SimonHansul <simonhansul@gmail.com>
2023-12-07 17:22:06 +0100

    comparison 1/LL2 vs LL2M

--- tests/loadtest.jl
+++ tests/loadtest.jl
@@ -12,9 +12,13 @@ p = (1., 3.)
 x = range(log(0.1), log(2), length = 100) |> collect |> x-> exp.(x)
 
 
-plot(x, 1 ./ [LL2(xi, p) for xi in x], x = "C_W", y = "y")
-plot(x, [LL2M(xi, p) for xi in x], x = "C_W", y = "y", xscale = :log10)
-vline!([1])
+p1 = plot(x = "C_W", y = "y", xlabel = "C_W", ylabel = "y", ylim = (0,Inf), leg = true, title = "1/LL2")
+[plot!(p1, x, 1 ./ [LL2(xi, (1., b)) for xi in x], label = "beta = $b") for b in [2., 3., 4., 5.]]
+
+p2 = plot(x = "C_W", y = "y", xlabel = "C_W", ylabel = "y", ylim = (0,Inf), title = "1-ln(LL2)")
+[plot!(p2, x, [LL2M(xi, (1., b)) for xi in x]) for b in [2., 3., 4., 5.]]
+
+plot(p1, p2) 
 
 
 
